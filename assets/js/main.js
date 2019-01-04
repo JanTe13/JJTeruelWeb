03e388a87f989ab373041a9de467a474548000b1
jan <jteruel@knapptic.com>
2019-01-04 11:50:11 +0100

    About Me completed with all translations and new information

--- assets/js/main.js
+++ assets/js/main.js
@@ -218,16 +218,11 @@ jQuery(document).ready(function ($) {
             en: "Industrial designer / Architect / Landscape architect"
         },
         "Description 1": {
-            cat: "(Barcelona 1952) és arquitecte per l'ETSAB (Escola Tècnica Superior d'Arquitectura de Barcelona) (2004), dissenyador industrial per l'escola Elisava (1978) i Graduat Superior en Paisatgisme per l'ETSAB (2009).",
-            es: "(Barcelona 1952) es arquitecto por la ETSAB (Escuela Técnica Superior de Arquitectura de Barcelona) (2004), diseñador industrial por la Escola Elisava (1978) y Graduado Superior en Paisajismo en la ETSAB (2009).",
-            en: "(Barcelona 1952) is an architect by ETSAB (The Barcelona Technical College of Architecture)(2004), Industrial Design by Escola Elisava (1978) and higher degree in Landscape at ETSAB (2009)."
+            cat: "(Barcelona 1952) és paisatgista, arquitecte i dissenyador industrial. Des de l'any 1984 dirigeix el seu propi estudi de disseny, arquitectura i paisatgisme amb una àmplia cartera de clients tant nacionals com internacionals, i una trajectòria reconeguda amb diversos premis i distincions, i referenciada en múltiples publicacions.",
+            es: "(Barcelona 1952) es paisagista, arquitecto y diseñador industrial. Desde el año 1984 dirige su propio estudio de diseño, arquitectura y paisagismo con una amplia cartera de clientes tanto nacionales como internacionales, y con una trayectoria reconocida con varios premios y distinciones, y referenciada en múltiples publicaciones.",
+            en: "(Barcelona 1952) is a landscape architect, architecht and industrial designer. Since 1984 he runs his own studio which counts on a wide portfolio of clients, both national and international, and a professional trajectory recognized with various prizes and distinctions, and references in diverse publications."
         },
         "Description 2": {
-            cat: "Dirigeix, des de l'any 1984, el seu propi estudi professional on desenvolupa projectes de disseny industrial, arquitectura i paisatgisme.",
-            es: "Dirige, desde el año 1984, su propio estudio profesional donde desarrolla proyectos de diseño industrial, arquitectura y paisajismo.",
-            en: "Since 1984 he runs his own industrial design studio, which counts on a wide portfolio of clients, both national and international, and a professional trajectory recognized with various prizes and distinctions, and references in diverse publications."
-        },
-        "Description 3": {
             cat: "Des de l'any 1996 és professor associat de la Facultat de Belles Arts de la Universitat de Barcelona i des de l'any 2011 és professor de BAU Escola Superior de Disseny de Barcelona.",
             es: "Desde el año 1996 es profesor asociado de la Facultat de Bellas Artes de la Universidad de Barcelona y desde el año 2011 es profesor de BAU Escuela Superior de Diseño de Barcelona.",
             en: "Since 1996 he is a lecturer at Faculty of Fine Arts, University of Barcelona, and since 2011 at the BAU, Design school in Barcelona."
@@ -236,10 +231,105 @@ jQuery(document).ready(function ($) {
             cat: "Dona un cop d'ull als productes dissenyats per ",
             es: "Hecha un vistazo a los productos  diseñados por ",
             en: "Have a look through the porducts designed by "
+        },
+        "Graduat Paisatgisme": {
+            cat: "Graduat Superior en Paisatgisme",
+            es: "Graduado Superior en Paisagismo",
+            en: "Higher Degree in Landscaping"
+        },
+        "Escola Arq": {
+            cat: "Escola Tècnica Superior d'Arquitectura",
+            es: "Escuela Técnica Superior de Arquitectura",
+            en: "Barcelona Technical College of Architecture"
+        },
+        "Arquitecte": {
+            cat: "Arquitecte",
+            es: "Arquitecto",
+            en: "Architect"
+        },
+        "DIndustrial": {
+            cat: "Dissenyador Industrial",
+            es: "Diseñador Industrial",
+            en: "Industrial Designer"
+        },
+        "Escola Elisava": {
+            cat: "Escola Elisava",
+            es: "Escuela Elisava",
+            en: "Elisava College"
+        },
+        "ArtsGraf": {
+            cat: "Graduat en Arts Gràfiques",
+            es: "Graduado en Artes Gráficas",
+            en: "Graduated in Graphic Arts"
+        },
+        "Escola Arts": {
+            cat: "Escola d'Arts Aplicades i d'Oficis Artístics",
+            es: "Escuela de Artes Aplicadas y de Oficios Artísticos",
+            en: "School of Applied Arts and Artistic Crafts"
+        },
+        "Prizes": {
+            cat: "Premis",
+            es: "Premios",
+            en: "Prizes"
+        },
+        "Others": {
+            cat: "Altres",
+            es: "Otros",
+            en: "Others"
+        },
+        "Iberdiseno": {
+            cat: '1990 Selecció "Iberdiseño 90"',
+            es: '1990 Selección "Iberdiseño 90"',
+            en: '1990 "Iberdiseño 90" selection'
+        },
+        "SIDI": {
+            cat: "1986-1987-1988-1990 Selecció SIDI",
+            es: "1986-1987-1988-1990 Selección SIDI",
+            en: "1986-1987-1988-1990 SIDI Selection"
+        },
+        "Valencia": {
+            cat: "València",
+            es: "Valencia",
+            en: "Valencia"
+        },
+        "Delta": {
+            cat: "1986, 2 Seleccions Delta ADI-FAD",
+            es: "1986, 2 Selecciones Delta ADI-FAD",
+            en: "1986, 2 Delta ADI-FAD Selections"
+        },
+        "Marie-Claire": {
+            cat: '1986, Selecció "La Maison de Marie-Claire"',
+            es: '1986, Selección "La Maison de Marie-Claire"',
+            en: '1986, "La Maison de Marie-Claire" Selection'
+        },
+        "Paris": {
+            cat: "París",
+            es: "París",
+            en: "Paris"
+        },
+        "Colegiat": {
+            cat: "Col·legiat",
+            es: "Colegiado",
+            en: "Collegiate"
+        },
+        "Soci": {
+            cat: "Soci",
+            es: "Socio",
+            en: "Partner"
+        },
+        "COAC": {
+            cat: " Col·legi Oficial d'Arquitectes de Catalunya (COAC)",
+            es: " Colegio Oficial de Arquitectos de Cataluña (COAC)",
+            en: " Col·legi Oficial d'Arquitectes de Catalunya (COAC)"
+        },
+        "ADI-FAD": {
+            cat: " Agrupació de Disseny Industrial del Foment de les Arts i el Disseny (ADI-FAD)",
+            es: " Agrupación de Diseño Industrial del Fomento de las Artes y el Diseño (ADI-FAD)",
+            en: " Agrupació de Disseny Industrial del Foment de les Arts i el Disseny (ADI-FAD)"
         }
     };
 
-    var translator = $('body').translate({lang: "cat", t: dict}); //use English
+    var translator = $('body').translate({lang: "cat", t: dict}); //use Catalan by default
 
     $('#en').click(function() {
         translator.lang("en");
